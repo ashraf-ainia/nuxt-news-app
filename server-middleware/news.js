ac06e217aeaf8a205860d2b0a68c8e985f8c34d3
Ashraf <ashrafainia1995@gmail.com>
2021-07-03 01:29:20 +0300

    feat: use env variable for API key

--- server-middleware/news.js
+++ server-middleware/news.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 
 app.post('/getArticles', (req, res) => {
   const reqBody = req.body;
-  request(`https://newsapi.org/v2/everything?q=${reqBody.searchText}&from=${reqBody.from}&to=${reqBody.to}&sortBy=popularity&apiKey=28ffa31431614b7aa2cb9a828c4740c3`, (error, response, body) => {
+  request(`https://newsapi.org/v2/everything?q=${reqBody.searchText}&from=${reqBody.from}&to=${reqBody.to}&sortBy=popularity&apiKey=${process.env.NUXT_ENV_NEWS_API_KEY}`, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       res.send(body);
     }
